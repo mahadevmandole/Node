053b7a6d50e8d7423e76ef59b4206f203083f757
mahadevmandole <mahadevmm77@gmail.com>
2026-04-13 23:42:18 +0530

    setup basic server

--- dev-tinder-node/src/app.js
+++ dev-tinder-node/src/app.js
@@ -1,1 +1,19 @@
-console.log("dev tinder")
+const express = require("express")
+const app = express()
+
+app.get("/",(req,res)=>{
+    console.log("here")
+    res.send("Hello from server")
+})
+
+app.use("/test",(req,res)=>{
+    res.send("Hello from test")
+})
+
+app.use("/hello",(req,res)=>{
+    res.send("Hello from hello")
+})
+
+app.listen(3000, ()=> {
+    console.log("Server listening")
+})
